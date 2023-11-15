153bbac736d1ac34d14df6454496f9aaba2fbb7b
binary_cat <v01z@mail.ru>
2023-11-15 14:37:25 +0300

    Project started

---
+++ Source/DungeonRaider/DungeonRaiderGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DungeonRaiderGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADungeonRaiderGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADungeonRaiderGameMode();
+};
+
+
+
